2c2990cd9b798f634db8dd7a9891e26b809af6dd
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-05-05 20:05:47 +0500

    feat: stream file

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
@@ -9,62 +11,64 @@ import (
 	"time"
 )
 
-type FileServer struct {}
+type FileServer struct{}
 
 func (fs *FileServer) start() {
-    ln, err := net.Listen("tcp", ":3000")
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            log.Fatal(err)
-        }
-        go fs.readloop(conn)
-    }
+	ln, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go fs.readloop(conn)
+	}
 }
 
-func (fs *FileServer) readloop(conn net.Conn)  {
-    buf := make([]byte, 2048)
-    for {
-        n, err := conn.Read(buf)
-        if err != nil {
-            log.Fatal(err)
-        }
-        file := buf[:n]
-        fmt.Println(file)
-        fmt.Printf("Recieved %d bytes\n", n)
-    }
+func (fs *FileServer) readloop(conn net.Conn) {
+	buf := new(bytes.Buffer)
+	for {
+		var size int64
+		binary.Read(conn, binary.LittleEndian, &size)
+		n, err := io.CopyN(buf, conn, size)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(buf.Bytes())
+		fmt.Printf("Recieved %d bytes\n", n)
+	}
 }
 
 func sendFile(size int) error {
-    file := make([]byte, size)
+	file := make([]byte, size)
 
-    _, err := io.ReadFull(rand.Reader, file)
-    if err != nil {
-        return err
-    }
+	_, err := io.ReadFull(rand.Reader, file)
+	if err != nil {
+		return err
+	}
 
-    conn, err := net.Dial("tcp", ":3000")
-    if err != nil {
-        return err
-    }
+	conn, err := net.Dial("tcp", ":3000")
+	if err != nil {
+		return err
+	}
 
-    n, err := conn.Write(file)
-    if err != nil {
-        return err
-    }
+	binary.Write(conn, binary.LittleEndian, int64(size))
+	n, err := io.CopyN(conn, bytes.NewReader(file), int64(size))
+	if err != nil {
+		return err
+	}
 
-    fmt.Printf("written %d bytes\n", n)
-    return nil
+	fmt.Printf("written %d bytes\n", n)
+	return nil
 }
 
 func main() {
-    go func ()  {
-        time.Sleep(time.Second * 4)
-        sendFile(1000)
-    } ()
-    fs := &FileServer{}
-    fs.start()
+	go func() {
+		time.Sleep(time.Second * 4)
+		sendFile(4000)
+	}()
+	fs := &FileServer{}
+	fs.start()
 }
